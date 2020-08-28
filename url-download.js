d2cefd1797c7e376ac0980f7e95dc29b2728d233
Anton Lenev <m0rg0t.Anton@gmail.com>
2020-08-28 23:20:52 +0300

    Added optional filename param for setting name of file we want to download in browser's dialog box

--- url-download.js
+++ url-download.js
@@ -1,13 +1,18 @@
-export default function urlDownload(fileUrl) {
+/**
+ * Open browser download dialog using <a> dwnload attribute
+ * @param fileUrl - url to file we want to download
+ * @param filename - optionla filename with which we should save file
+ */
+export default function urlDownload(fileUrl, filename = '') {
     var tempLink = document.createElement('a');
     tempLink.style.display = 'none';
     tempLink.href = fileUrl;
-    tempLink.setAttribute('download', fileUrl);
+    tempLink.setAttribute('download', filename || fileUrl);
 
     // Safari thinks _blank anchor are pop ups. We only want to set _blank
     // target if the browser does not support the HTML5 download attribute.
     // This allows you to download files in desktop safari if pop up blocking
-    // is enabled.
+    // is enabled.npm install --save-dev eslint
     if (typeof tempLink.download === 'undefined') {
         tempLink.setAttribute('target', '_blank');
     }
